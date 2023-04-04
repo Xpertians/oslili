afc875a02f26fe286232a5c03066ef3a5ebc0e40
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2023-04-04 01:17:36 -0700

    updating license

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='license-identifier',
-    version='0.1',
+    name="license-identifier", version="0.1.3",
+    description="Open Source License Identification Library",
+    author="Andrew Barrier",
+    author_email="alkamod@protonmail.com",
     packages=find_packages(),
-    install_requires=[
-        'scikit-learn',
-    ],
+    install_requires=["scikit-learn", ],
+    url='https://your-project-url.com',
     package_data={
         '': ['*.txt'],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        ],
 )
