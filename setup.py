6afb3ba55205b8d9fb2cdebd86b553ee24c7c013
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2023-04-07 16:16:26 -0700

    pump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="oslili", version="0.1",
+    name="oslili", version="0.2",
     description="Open Source License Identification Library",
     author="Oscar Valenzuela",
     author_email="oscar.valenzuela.b@gmail.com",
