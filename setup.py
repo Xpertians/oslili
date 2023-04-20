01142c084b3428a5dc51e54a197bd88d25212fa3
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2023-04-20 15:06:34 +0200

    test

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
+import pathlib
+import sys
 from setuptools import setup, find_packages
 
+
+HERE = pathlib.Path(__file__).parent
+VERSION = '0.6'
+PACKAGE_NAME = 'oslili'
+AUTHOR = 'Oscar Valenzuela B.'
+AUTHOR_EMAIL = 'oscar.valenzuela.b@gmail.com'
+URL = 'https://github.com/oscarvalenzuelab/oslili'
+LICENSE = 'Apache-2.0'
+DESCRIPTION = 'Open Source License Identification Library'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
 setup(
-    name="oslili", version="0.3",
-    description="Open Source License Identification Library",
-    author="Oscar Valenzuela",
-    author_email="oscar.valenzuela.b@gmail.com",
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    license=LICENSE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
     packages=['oslili', 'oslili.spdx'],
     install_requires=["scikit-learn", ],
-    url='https://opensourcelicensecompliance.com',
+    url=URL,
     package_data={
         "oslili.spdx": ["*.txt",],
     },
