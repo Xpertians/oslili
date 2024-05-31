a01d00602a1b4d046d63dbb185d07d5bccb8144c
Oscar V <oscar.valenzuela.b@gmail.com>
2024-05-30 22:46:15 -0700

    version pump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 HERE = pathlib.Path(__file__).parent
-VERSION = '0.12'
+VERSION = '0.13'
 PACKAGE_NAME = 'oslili'
 AUTHOR = 'Oscar Valenzuela B.'
 AUTHOR_EMAIL = 'alkamod@gmail.com'
